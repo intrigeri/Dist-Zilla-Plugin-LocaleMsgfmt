170f15fdfc5844c2571c9eb32a276e9850196b6c
Jérôme Quelin <jquelin@gmail.com>
2011-05-20 14:41:25 +0200

    pod & comment updates

--- lib/Dist/Zilla/Plugin/LocaleMsgfmt.pm
+++ lib/Dist/Zilla/Plugin/LocaleMsgfmt.pm
@@ -1,37 +1,22 @@
 package Dist::Zilla::Plugin::LocaleMsgfmt;
-# ABSTRACT: Dist::Zilla plugin that compiles Local::Msgfmt .po files to .mo files
-
-=head1 DESCRIPTION
-
-Put the following in your dist.ini
-
-    [LocaleMsgfmt]
-    locale = share/locale ;; (optional)
-
-This plugin will compile all of the .po files it finds in the locale directory into .mo
-files, via Locale::Msgfmt.
-
-=head1 TODO
-
-Remove the generated files after the build finishes, or better yet do the generation inside
-the build dir.
-
-=cut
+# ABSTRACT: compiles .po files to .mo files with Local::Msgfmt
 
 use Locale::Msgfmt 0.14;
 use Moose;
+use Path::Class;
+
 with 'Dist::Zilla::Role::BeforeBuild';
 
-=method mvp_multivalue_args
 
-For Config::MVP - specify setting names that may have multiple values and that will always
-be stored in an arrayref
+# -- attributes
 
-=cut
-
+# For Config::MVP - specify setting names that may have multiple values and that will always
+# be stored in an arrayref
 sub mvp_multivalue_args { qw(locale) }
 
 =attr locale
+
+Path to the directory containing the locale files.
 
 =cut
 
@@ -40,29 +25,25 @@ has locale => (
     isa     => 'ArrayRef[Str]',
     lazy    => 1,
     default => sub {
-        my ($self) = @_;
-        my $path = File::Spec->catfile( $self->zilla->root, 'share', 'locale' );
-        if ( -e $path ) {
-            return [$path];
-        }
-        else {
-            return [];
-        }
+        my $self = shift;
+        my $path = dir( $self->zilla->root, 'share', 'locale' );
+        return -e $path ? [ $path ] : [ ];
     },
 );
 
-=method before_build
 
-=cut
+# -- public methods
 
+#
+# to implement Dist::Zilla::Role::BeforeBuild
 sub before_build {
     my ( $self, $args ) = @_;
 
     for my $dir ( @{ $self->locale } ) {
-        my $path = Path::Class::Dir->new($dir);
+        my $path = dir($dir);
         if ( -e $path ) {
             $self->log("Generating .mo files from .po files in $path");
-            Locale::Msgfmt::msgfmt( { in => $path->absolute, verbose => 1, remove => 0 } );
+            msgfmt( { in => $path->absolute, verbose => 1, remove => 0 } );
         }
         else {
             warn "Skipping invalid path: $path";
@@ -74,3 +55,25 @@ __PACKAGE__->meta->make_immutable;
 no Moose;
 
 1;
+__END__
+
+=for Pod::Coverage
+    before_build
+    mvp_multivalue_args
+
+
+=head1 DESCRIPTION
+
+Put the following in your S<F<dist.ini> :>
+
+    [LocaleMsgfmt]
+    locale = share/locale ; this is the default
+
+This plugin will compile all of the .po files it finds in the locale directory into .mo
+files, via Locale::Msgfmt.
+
+=head1 TODO
+
+Remove the generated files after the build finishes, or better yet do the generation inside
+the build dir.
+
